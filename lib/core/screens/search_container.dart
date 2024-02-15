a8633c5af6d29cb69dd54012a3d5ad0fe852f8a4
ulrichsi-hrbl <ulrichsi@herbalife.com>
2024-02-14 17:17:36 -0800

    updated with ‘search_user’

--- lib/core/screens/search_container.dart
+++ lib/core/screens/search_container.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:search_user/search_user.dart';
 
 class SearchContainer extends StatelessWidget {
   const SearchContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Search Container'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SearchUser(),
     );
   }
 }
